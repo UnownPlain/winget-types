be4a3b76fff2b2a7c9edf1495a9d8babc2370226
UnownPlain <git@unownplain.dev>
2025-10-29 22:59:59 -0400

    Add ValidFileExtensions enum

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,9 @@
+mod extensions;
 mod generic_manifest;
 pub mod language_tag;
 mod relative_dirs;
 pub mod sha_256;
 
+pub use extensions::ValidFileExtensions;
 pub use generic_manifest::GenericManifest;
 pub use relative_dirs::RelativeDir;
